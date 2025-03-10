60cc52d6d633a217d5c9b61f4dfe9add18f065f6
Pierre-Clayton <pierre.clayton.pro@gmail.com>
2025-03-10 15:46:23 +0100

    Ecrire un message ici par ex: Adding utils.com for outlier detection

---
+++ test.py
@@ -0,0 +1,3 @@
+print("Si je de base j'ai un fichier vide et je rajoute quelque chose, je vais le push pour te montrer le chemin. DOnc la supposons que j'ai fini de coder mon fichier."
+"Je commence par enregistrezr mon fichier (ctrl + s)")
+#Quand c'est enregistré il n'y a plus le point blanc. 
